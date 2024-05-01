7a05c228f4033181cf2f5685b4319e7cdf9d3e36
Behl <aayush.behl@ubc.ca>
2024-04-30 18:38:50 -0700

    App info ssm paramters added

---
+++ amplify/backend/custom/backendEnvParameter/cdk-stack.ts
@@ -0,0 +1,22 @@
+import * as cdk from 'aws-cdk-lib';
+import * as AmplifyHelpers from '@aws-amplify/cli-extensibility-helper';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
+import { AmplifyDependentResourcesAttributes } from '../../types/amplify-dependent-resources-ref';
+import { Construct } from 'constructs';
+
+export class cdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps, amplifyResourceProps?: AmplifyHelpers.AmplifyResourceProps) {
+    super(scope, id, props);
+    /* Do not remove - Amplify CLI automatically injects the current deployment environment in this input parameter */
+    new cdk.CfnParameter(this, 'env', {
+      type: 'String',
+      description: 'Current Amplify CLI env name',
+    });
+    /* AWS CDK code goes here - learn more: https://docs.aws.amazon.com/cdk/latest/guide/home.html */
+    
+    new ssm.StringParameter(this, 'ParameterBackendName', {
+      parameterName: 'amplify/appInfo/envName',
+      stringValue: cdk.Fn.ref('env')
+    })
+  }
+}
